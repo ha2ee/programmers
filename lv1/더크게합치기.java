c90952655642b22ef4ccbdef590533a147703b83
KimDongHyun <donghyun.develop@gmail.com>
2023-05-08 15:01:29 +0900

    2023.05.08 Day3 연산

---
+++ lv1/더크게합치기.java
@@ -0,0 +1,21 @@
+package lv1;
+
+public class 더크게합치기 {
+    public static void main(String[] args) {
+        
+    }
+
+    class Solution {
+        public int solution(int a, int b) {
+            int answer = a+b;
+            int temp1 = Integer.parseInt(String.valueOf(a).toString()+String.valueOf(b).toString());
+            int temp2 = Integer.parseInt(String.valueOf(b).toString()+String.valueOf(a).toString());
+            if(answer > temp1 && answer > temp2){
+                return answer;
+            } else {
+                return temp1 > temp2 ? temp1 : temp2;
+            }
+
+        }
+    }
+}
--- lv1/특수문자출력.java
+++ lv1/특수문자출력.java
@@ -1,4 +1,4 @@
-package programmers;
+package lv1;
 public class 특수문자출력 {
     public static void main(String[] args) {
         System.out.println("!@#$%^&*(\\'\"<>?:;");
