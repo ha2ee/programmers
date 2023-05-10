4480bcd1c2ff529aeef79146fcf7fd3dd3aa6665
KimDongHyun <donghyun.develop@gmail.com>
2023-05-10 15:07:46 +0900

    2023.05.10 Day7 반복문

---
+++ lv1/배열만들기2.java
@@ -0,0 +1,36 @@
+package lv1;
+
+public class 배열만들기2 {
+    public static void main(String[] args) {
+    int[] arr = solution(10, 20);
+    for(int i : arr) {
+        System.out.println(i);
+    }
+    }
+    public static int[] solution(int l, int r) {
+        int[] answer = {};
+        int[] answertemp = new int[r-l];
+        int idx = 0;
+        StringBuffer sb = new StringBuffer();
+        for(int i = l; i<=r; i++) {
+            String temp = "";
+            temp = ""+i;
+            String reg = "^[50]+$";
+            if(temp.matches(reg)){
+                sb.setLength(0);
+                sb.append(temp);
+                answertemp[idx] = Integer.parseInt(sb.toString());
+                idx++;
+            }
+        }
+        answer = new int[idx];
+        for(int i =0; i<idx; i++) {
+            answer[i] = answertemp[i];
+        }
+        if(sb.length() == 0) {
+            answer = new int[1];
+            answer[0] = -1;
+        }
+            return answer;
+        }
+}
