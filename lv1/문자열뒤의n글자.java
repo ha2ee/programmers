94f832248687ca9764d3801665b8317032a3bb27
KimDongHyun <donghyun.develop@gmail.com>
2023-05-11 22:59:37 +0900

    2023.05.11 Day9 문자열

---
+++ lv1/문자열뒤의n글자.java
@@ -0,0 +1,13 @@
+package lv1;
+
+public class 문자열뒤의n글자 {
+    public static void main(String[] args) {
+        
+    }
+    public String solution(String my_string, int n) {
+        String answer = "";
+        answer = my_string.substring(my_string.length()-n);
+        
+        return answer;
+    }
+}
