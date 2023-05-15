6f4a6d6c081d9cceb875408065280aa458f378e0
KimDongHyun <donghyun.develop@gmail.com>
2023-05-15 20:30:55 +0900

    2023.05.15 Day12 리스트(배열)

---
+++ lv1/첫번째로나오는음수.java
@@ -0,0 +1,17 @@
+package lv1;
+
+public class 첫번째로나오는음수 {
+    public static void main(String[] args) {
+        
+    }
+
+    public int solution(int[] num_list) {
+        int answer = -1;
+        for(int i =0; i<num_list.length; i++){
+            if(num_list[i]<0){
+                return answer = i;
+            }
+        }
+        return answer;
+    }
+}
