7cce4be8d26cdb4722c11a85de862d022f296911
KimDongHyun <donghyun.develop@gmail.com>
2023-05-15 12:03:05 +0900

    2023.05.15 Day11 리스트(배열)

---
+++ lv1/글자지우기.java
@@ -0,0 +1,30 @@
+package lv1;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class 글자지우기 {
+    public static void main(String[] args) {
+        int[] indices = {1, 16, 6, 15, 0, 10, 11, 3};
+        System.out.println(solution("apporoograpemmemprs", indices));
+    }
+
+    public static String solution(String my_string, int[] indices) {
+        String answer = "";
+        List<String> strList = new ArrayList<>(Arrays.asList(my_string.split("")));
+        List<Integer> intList =  Arrays.stream(indices)
+            .boxed()
+            .collect(Collectors.toList());
+        intList.sort(Collections.reverseOrder());
+        for(int i : intList){
+            strList.remove(i);
+        }
+        for(String s : strList){
+            answer += s;
+        }
+        return answer;
+    }
+}
