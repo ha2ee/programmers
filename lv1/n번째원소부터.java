05f805bf6f39bf048c3cb98c4bfabf590bc1a87d
KimDongHyun <donghyun.develop@gmail.com>
2023-05-16 13:40:17 +0900

    2023.05.16 Day13 리스트(배열)

---
+++ lv1/n번째원소부터.java
@@ -0,0 +1,15 @@
+package lv1;
+
+public class n번째원소부터 {
+    public static void main(String[] args) {
+        
+    }
+    public int[] solution(int[] num_list, int n) {
+        int[] answer = new int[num_list.length-n+1];
+        
+        for(int i = 0, j = n; j<=num_list.length; j++, i++){
+            answer[i]=num_list[j-1];
+        }
+        return answer;
+    }
+}
