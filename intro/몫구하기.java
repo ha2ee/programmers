397429d6d7088b3bd4aec9f19f8b678a9347ce70
KimDongHyun <donghyun.develop@gmail.com>
2023-05-23 13:48:18 +0900

    2023.05.23 코딩테스트입문 Day1 사칙연산

---
+++ intro/몫구하기.java
@@ -0,0 +1,12 @@
+package intro;
+
+public class 몫구하기 {
+    public static void main(String[] args) {
+        
+    }
+
+    public int solution(int num1, int num2) {
+        int answer = num1/num2;
+        return answer;
+    }
+}
